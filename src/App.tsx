6e6de65e55909aa7ad9ffc4e205ba79c9d8a3a0d
Andrey <as.odinson@gmail.com>
2023-10-02 21:34:40 +0300

    feat: update redux to redux/toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import UserList from './components/UserList';
+import { useSelector } from 'react-redux';
+import { useAppSelector } from './store/hooks/redux';
+import { userSlice } from './store/reducers/userSlice';
+import { useDispatch } from 'react-redux';
 
 function App() {
+  const { count } = useAppSelector((state) => state.userReducer);
+  const { increment } = userSlice.actions;
+  const dispatch = useDispatch();
+  console.log(increment);
   return (
     <div>
-      <UserList />
+      <h1>{count}</h1>
+      {/* <button onClick={() => dispatch(increment())}>+</button> */}
     </div>
   );
 }
