fa5ea01c931dd4fbde962a7d39ced9ef4c703ae0
Andrey <as.odinson@gmail.com>
2023-10-03 12:03:45 +0300

    feat: update fetching data

--- src/store/reducers/userSlice.ts
+++ src/store/reducers/userSlice.ts
@@ -1,5 +1,6 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 import { IUser } from '../models/IUser';
+import { fetchUsers } from './actionCreator';
 
 interface UserState {
   users: IUser[];
@@ -18,21 +19,22 @@ const initialState: UserState = {
 export const userSlice = createSlice({
   name: 'user',
   initialState,
-  reducers: {
-    userFetching(state) {
-      state.isLoading = true;
-    },
-    userFetchingSuccess(state, action:PayloadAction<IUser[]>) {
+  reducers: {},
+  extraReducers: {
+    [fetchUsers.fulfilled.type]: (state, action: PayloadAction<IUser[]>) => {
       state.isLoading = false;
-      state.error ='';
+      state.error = '';
       state.users = action.payload;
     },
-    userFetchingFailing(state, action) {
+    [fetchUsers.pending.type]: (state) => {
+      state.isLoading = true;
+    },
+    [fetchUsers.rejected.type]: (state, action) => {
       state.isLoading = false;
       state.error = action.payload;
     },
-
   },
 });
 
 export default userSlice.reducer;
+
