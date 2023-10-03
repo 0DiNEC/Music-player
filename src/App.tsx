eac20b9d917d7d18a321c9ce8c9d5f947137b990
Andrey <as.odinson@gmail.com>
2023-10-03 12:41:13 +0300

    feat: display posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import React, { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from './store/hooks/redux';
 import { fetchUsers } from './store/reducers/actionCreator';
+import PostContainer from './components/PostContainer';
 
 function App() {
-  const dispatch = useAppDispatch();
-  const { users, isLoading, error } = useAppSelector(
-    (state) => state.userReducer,
-  );
-  useEffect(() => {
-    dispatch(fetchUsers());
-  }, []);
+  // const dispatch = useAppDispatch();
+  // const { users, isLoading, error } = useAppSelector(
+  //   (state) => state.userReducer,
+  // );
+  // useEffect(() => {
+  //   dispatch(fetchUsers());
+  // }, []);
   return (
     <div>
-      {isLoading && <h1> Loading...</h1>}
-      {isLoading && <h1> {error}</h1>}
-      {JSON.stringify(users, null, 2)}
+      <PostContainer />
     </div>
   );
 }
